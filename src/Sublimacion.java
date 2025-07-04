390e2342998c3b6e94f23d0a7b520df4eee5ad1d
jorgomque <jorgomque@alu.edu.gva.es>
2025-07-04 11:49:04 +0200

    creacion de repositorio y clases basicas

---
+++ src/Sublimacion.java
@@ -0,0 +1,13 @@
+public class Sublimacion {
+    private String Nombre;
+    private String Combinacion;
+    private String Categoria;
+    private String Efecto;
+
+    public Sublimacion(String nombre, String combinacion, String categoria, String efecto) {
+        Nombre = nombre;
+        Combinacion = combinacion;
+        Categoria = categoria;
+        Efecto = efecto;
+    }
+}
