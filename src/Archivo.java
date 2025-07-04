f4fa32928e46c9b579b37a0e63b5c311cac43fb2
jorgomque <jorgomque@alu.edu.gva.es>
2025-07-04 19:50:40 +0200

    clase Sublimacion completa

--- src/Archivo.java
+++ src/Archivo.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 public class Archivo {
-    static private ArrayList<Sublimacion> lista;
+     private ArrayList<Sublimacion> lista;
 
+    public Archivo() {
+        this.lista = new ArrayList<>();
+    }
 
-
+    public void nuevaSubli(String nombre, String combinacion, String categoria, String efecto){
+        this.lista.add(new Sublimacion(  nombre,  combinacion,  categoria, efecto));
+    }
 }
--- src/Sublimacion.java
+++ src/Sublimacion.java
@@ -1,13 +1,51 @@
 public class Sublimacion {
-    private String Nombre;
-    private String Combinacion;
-    private String Categoria;
-    private String Efecto;
+    private String nombre;
+    private String combinacion;
+    private String categoria;
+    private String efecto;
 
     public Sublimacion(String nombre, String combinacion, String categoria, String efecto) {
-        Nombre = nombre;
-        Combinacion = combinacion;
-        Categoria = categoria;
-        Efecto = efecto;
+        setNombre(nombre);
+        setCombinacion(combinacion);
+        setCategoria(categoria);
+        setEfecto(efecto);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCombinacion() {
+        return combinacion;
+    }
+
+    public void setCombinacion(String combinacion) {
+        this.combinacion = combinacion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getEfecto() {
+        return efecto;
+    }
+
+    public void setEfecto(String efecto) {
+        this.efecto = efecto;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre de la subli: " + getNombre() + " combinacion de colores: "
+                + getCombinacion() + " efecto: " + getEfecto() + " categoria: " + getCategoria();
     }
 }
